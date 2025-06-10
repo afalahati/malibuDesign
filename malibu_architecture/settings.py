98f799386a4259c05380a5e27fd06fb3965f45af
Amir <a.falahati@live.com>
2025-06-11 00:43:03 +0330

    a

--- malibu_architecture/settings.py
+++ malibu_architecture/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5(w60ombx3_6sf0xx*u7!t-+k00z$nccn54nt-swdi1pdyhqzq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
