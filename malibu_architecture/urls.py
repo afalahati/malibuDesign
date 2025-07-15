4cdeadf2db0f8c8d0afc8d3ca24819be9bf87774
Amir <a.falahati@live.com>
2025-07-15 15:07:31 +0330

    final commit

--- malibu_architecture/urls.py
+++ malibu_architecture/urls.py
@@ -7,6 +7,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.sitemaps.views import sitemap
 from main.sitemaps import BlogPostSitemap
+from django.views.generic import TemplateView
 
 sitemaps_dict = {
     'blog': BlogPostSitemap,
@@ -17,6 +18,7 @@ urlpatterns = [
     path('', include('main.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps_dict}, name='sitemap'),
+    path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
 ]
 # اضافه کردن این خط برای سرو فایل‌های رسانه‌ای
 if settings.DEBUG:
