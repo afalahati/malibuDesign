8d96f83ba3d6c0e2c5191c126aea4cad68686e83
Amir <a.falahati@live.com>
2025-08-14 00:50:28 +0330

    14040523

--- malibu_architecture/settings.py
+++ malibu_architecture/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5(w60ombx3_6sf0xx*u7!t-+k00z$nccn54nt-swdi1pdyhqzq'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-5(w60ombx3_6sf0xx*u7!t-+k00z$nccn54nt-swdi1pdyhqzq')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
