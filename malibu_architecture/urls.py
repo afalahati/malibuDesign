fc50e7bd0f923c3c076470fff2ac0e47ee565e3a
Amir <a.falahati@live.com>
2025-08-14 10:36:41 +0330

    14040523

--- malibu_architecture/urls.py
+++ malibu_architecture/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
 ]
 # اضافه کردن این خط برای سرو فایل‌های رسانه‌ای
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
